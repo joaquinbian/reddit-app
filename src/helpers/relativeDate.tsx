7d92459e27bee4cae051a46a8e7a8b790eda14b6
joaquinbian <joacobian21@gmail.com>
2021-09-05 21:53:29 -0300

    change in relative date

--- src/helpers/relativeDate.tsx
+++ src/helpers/relativeDate.tsx
@@ -4,23 +4,32 @@ export const relativeDate = (time: number) => {
   const secondsPast = Date.now() / 1000 - seconds;
   const minutes = secondsPast / 60;
 
+  let result;
+
   if (secondsPast < 60) {
-    return `${Math.floor(secondsPast)} seconds ago`;
+    result = Math.floor(secondsPast);
+    return result === 1 ? `${result} second ago` : `${result} seconds ago`;
   } else if (secondsPast < 3600) {
-    return `${Math.floor(secondsPast / 60)} minutes ago`;
+    result = Math.floor(secondsPast / 60);
+    return result === 1 ? `${result} minute ago` : `${result} minutes ago`;
   } else if (secondsPast < 86400) {
-    return `${Math.floor(secondsPast / 3600)} hours ago`;
+    result = Math.floor(secondsPast / 3600);
+    return result === 1 ? `${result} hour ago` : `${result} hours ago`;
   } else if (secondsPast < 604800) {
-    return `${Math.floor(secondsPast / 86400)} days ago`;
+    result = Math.floor(secondsPast / 86400);
+    return result === 1 ? `${result} day ago` : `${result} days ago`;
   } else if (minutes < 43800) {
     //paso de segundos a minutos porque es un numero muy grande en seg.
     //43800 son los minutos en un mes, si es menor es q estamos en semanas
-    return `${Math.floor(secondsPast / 604800)} weeks ago`;
+    result = Math.floor(secondsPast / 604800);
+    return result === 1 ? `${result} week ago` : `${result} weeks ago`;
   } else if (minutes < 525600) {
     //en un año hay 525600 minutos, si hay menos es pq estamos dentro del año
     // y sacamos los meses
-    return `${Math.floor(minutes / 43800)} months ago`;
+    result = Math.floor(minutes / 43800);
+    return result === 1 ? `${result} month ago` : `${result} months ago`;
   } else {
-    return `${Math.floor(minutes / 525600)} years ago`;
+    result = Math.floor(minutes / 525600);
+    return result === 1 ? `${result} year ago` : `${result} years ago`;
   }
 };
